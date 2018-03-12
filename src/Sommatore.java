1bb0447e65dadaad2abde43c2a67e7e2a5d3cbb2
Yuri Botticchio <yuribotticchio@gmail.com>
2018-03-12 12:30:26 +0100

    aggiunto primo commit

---
+++ src/Sommatore.java
@@ -0,0 +1,20 @@
+
+public class Sommatore 
+{
+	public static int somma4(int x)
+	{
+		return x+4;
+	}
+	
+	public static int somma10(int x)
+	{
+		return x+10;
+	}
+	
+	public static void main(String[] args) 
+	{
+		int x=1;
+		System.out.println(somma4(x));
+		System.out.println(somma10(x));
+	}
+}
